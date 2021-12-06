90ce42814137531bf847a494162843808dcc0fae
Chad McColm <chadjmccolm@gmail.com>
2021-12-06 12:18:12 -0800

    updated feedbacks and readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PLC_HMI_Backend
 This is the backend of a node.js application which will serve a client with data via TwinCAT ADS. 
+
+# Methods
+## Read All
+A GET request to `/` will return all of the values with Response Status 200. If an error occurs, it will return the JSON object: `{message: "Error Grabbing Data"}` with Response Status 400. 
+
+## Read One
+A GET request to `/symbol` will return the value of just that symbol with Response Status 200. If no symbol is requested, it will return the JSON object: `{message: "Error Grabbing Data"}` with Response Status 400. 
+
+## Write One
+A POST request to `/` with a body composed of JSON containing elements `symbol` and `value` such as `{"symbol" : "bStartButton", "value": "0"}` will update the data structure and return the result of the "Read All" method with Response Status 200. If either value is missing it will return Response Status 400 and JSON body: `{message: "Request Symbol/Value Empty"}`. If an internal error occurs it will return JSON body `{message: "Error Writing Data"}` with Response Status 500. 
--- controller.js
+++ controller.js
@@ -3,21 +3,32 @@ const db = require("./dataStructure");
 
 // Retrieve all Tutorials from the database.
 exports.readAll = (req, res) => {
-    console.log("read all function");
-    res.send(db);
+    try{
+        data = db;
+    }
+    catch (e) {
+        console.log(e);
+        res.status(400).send({message: "Error Grabbing Data"});
+    }
+    res.status(200).send(data);
 };
 
 // Find a single Tutorial with an id
 exports.readOne = (req, res) => {
-    console.log("read one function");
     // If no symbol parameter then request was empty
     if(!req.params.symbol){
         res.status(400).send({message: "Request Content Empty"});
         return;
     }
     output = {};
-    output[req.params.symbol] = db[req.params.symbol];
-    res.status(200).send(output);
+    if(db[req.params.symbol]){
+        output[req.params.symbol] = db[req.params.symbol];
+        res.status(200).send(output);
+    }
+    else{
+        res.status(400).send({message: "Error Grabbing Data"});
+    }
+    
 };
 
 // Write a symbols data to the ADS server
@@ -27,8 +38,19 @@ exports.writeOne = (req, res) => {
         res.status(400).send({message: "Request Symbol Empty"});
         return;
     }
+    else if(!req.body.value){
+        res.status(400).send({message: "Request Symbol Empty"});
+        return;
+    }
     else{
-        db[req.body.symbol] = req.body.value;
+        try{
+            db[req.body.symbol] = req.body.value;
+        }
+        catch(e){
+            console.log(e);
+            res.status(500).send({message: "Error Writing Data"});
+            return;
+        }
         res.status(200).send(db);
     }
 
