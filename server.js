60be1b1748955f3c2aecf5e283c78dc3ac707e7c
Chad McColm <chadjmccolm@gmail.com>
2021-12-10 20:21:46 -0800

    rename backend to src and abandoned some dreams

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 // set port, listen for requests
 const PORT = process.env.PORT || 80;
-require("./backend/controller.routes")(app);
+require("./src/controller.routes")(app);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
